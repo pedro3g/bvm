b39f07c0d1785e1728913398bc3c7d98866bbefa
Pedro Henrique <opedro3g@gmail.com>
2024-01-24 13:16:54 -0300

    fix(get_base_dir.go): fix error message to correctly state "BVM base dir not found" instead of "NVM base dir not found" for clarity and accuracy

--- utils/get_base_dir.go
+++ utils/get_base_dir.go
@@ -9,7 +9,7 @@ func GetBaseDir() (string, error) {
 	baseDir := os.Getenv("BVM_DIR")
 
 	if len(baseDir) == 0 {
-		return "", errors.New("NVM base dir not found")
+		return "", errors.New("BVM base dir not found")
 	}
 
 	return baseDir, nil
