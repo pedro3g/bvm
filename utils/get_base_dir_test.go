194c2cd10173681b01f8fdd5c4f9231646b9d7aa
Pedro Henrique <opedro3g@gmail.com>
2024-01-24 13:04:37 -0300

    feat(cli): add CLI functionality to print command line arguments
    feat(utils): add GetBaseDir function to retrieve NVM base directory
    
    test(utils): add unit test for GetBaseDir function

---
+++ utils/get_base_dir_test.go
@@ -0,0 +1,15 @@
+package utils_test
+
+import (
+	"testing"
+
+	"github.com/pedro3g/bvm/utils"
+)
+
+func TestGetBaseDir(t *testing.T) {
+	_, err := utils.GetBaseDir()
+
+	if err != nil {
+		t.Errorf("Expect for the NVM_GO variable to exist. %f", err)
+	}
+}
